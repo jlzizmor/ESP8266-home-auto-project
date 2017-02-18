3963f1ce3b553ef642370e8a5ecd27fbf1a3b3ae
jlzizmor <jlzizmor@gmail.com>
2017-02-18 21:32:36 +0000

    added python scripts

---
+++ google_home/public/python/lights-bedroom-closet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import paho.mqtt.publish as publish
+import sys
+
+topic = 	"lights/bedroom"
+msg_part = 	"closet "
+hostname =	"localhost"
+
+if len(sys.argv) == 2:
+	publish.single(topic, msg_part + sys.argv[1].lower(), hostname=hostname)
