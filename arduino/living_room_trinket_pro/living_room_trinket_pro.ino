382f50c41a84f7c9120232fd170a7bea6fed6a68
jlzizmor <jlzizmor@gmail.com>
2017-03-01 20:36:08 -0500

    added living room trinket script

---
+++ arduino/living_room_trinket_pro/living_room_trinket_pro.ino
@@ -0,0 +1,21 @@
+/*
+ * This script is for the trinket in the living room.
+ */
+
+#define DOOR_BUTTON 1
+#define SIDE_BUTTON 2
+#define LAMPS_BUTTON 3
+#define ALL_BUTTON 4
+#define DOOR_PWM 5
+#define SIDE_PWM 6
+#define LAMPS_PWM 9
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
