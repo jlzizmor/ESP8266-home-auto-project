fec73a137b52b3c323c67de92503ef08e19c1a89
jlzizmor <jlzizmor@gmail.com>
2017-03-01 21:07:52 -0500

    added setup()

--- arduino/living_room_trinket_pro/living_room_trinket_pro.ino
+++ arduino/living_room_trinket_pro/living_room_trinket_pro.ino
@@ -2,6 +2,8 @@
  * This script is for the trinket in the living room.
  */
 
+#include <Bounce2.h>
+
 #define DOOR_BUTTON 1
 #define SIDE_BUTTON 2
 #define LAMPS_BUTTON 3
@@ -9,13 +11,46 @@
 #define DOOR_PWM 5
 #define SIDE_PWM 6
 #define LAMPS_PWM 9
+#define DEBOUNCE_INTERVAL 50
+
+const String ON = "ON";
+const String OFF = "OFF";
+const String DOOR = "DOOR";
+const String SIDE = "SIDE";
+const String LAMPS = "LAMPS";
+const String ALL = "ALL";
+const char delimiter = ' ';
+
+Bounce doorBtn = Bounce();
+Bounce sideBtn = Bounce();
+Bounce lampsBtn = Bounce();
+Bounce allBtn = Bounce();
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  pinMode(DOOR_BUTTON, INPUT_PULLUP);
+  pinMode(SIDE_BUTTON, INPUT_PULLUP);
+  pinMode(LAMPS_BUTTON, INPUT_PULLUP);
+  pinMode(ALL_BUTTON, INPUT_PULLUP);
+  pinMode(DOOR_PWM, OUTPUT);
+  pinMode(SIDE_PWM, OUTPUT);
+  pinMode(LAMPS_PWM, OUTPUT);
+
+  doorBtn.attach(DOOR_BUTTON);
+  sideBtn.attach(SIDE_BUTTON);
+  lampsBtn.attach(LAMPS_BUTTON);
+  allBtn.attach(ALL_BUTTON);
+
+  doorBtn.interval(DEBOUNCE_INTERVAL);
+  sideBtn.interval(DEBOUNCE_INTERVAL);
+  lampsBtn.interval(DEBOUNCE_INTERVAL);
+  allBtn.interval(DEBOUNCE_INTERVAL);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
